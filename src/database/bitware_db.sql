5e8281b5c183e8a9b93ca080dd830befcf9be7f6
Hércules <herculespereira99@gmail.com>
2025-08-28 21:15:48 -0300

    Atualização do script do banco de dados

--- src/database/bitware_db.sql
+++ src/database/bitware_db.sql
@@ -1,38 +1,51 @@
+drop database bitware_db;
 CREATE DATABASE bitware_db;
 USE bitware_db;
+
+CREATE TABLE bitware_db.Usuario (
+	idUsuario INT AUTO_INCREMENT,
+	email VARCHAR(200) NOT NULL UNIQUE,
+    senha VARCHAR(200) NOT NULL,
+    nivelAcesso INT NOT NULL,
+    PRIMARY KEY (idUsuario)
+);
 
 CREATE TABLE bitware_db.Empresa (
   idEmpresa INT NOT NULL AUTO_INCREMENT,
   cnpj VARCHAR(14) NOT NULL,
-  PRIMARY KEY (idEmpresa)
+  nome VARCHAR(200) NOT NULL,
+  fkUsuario INT NOT NULL,
+  PRIMARY KEY (idEmpresa),
+  CONSTRAINT fk_Usuario_Empresa
+    FOREIGN KEY (fkUsuario)
+    REFERENCES bitware_db.Usuario (idUsuario)
 );
 
 CREATE TABLE bitware_db.Cargo (
   idCargo INT NOT NULL AUTO_INCREMENT,
   descricao VARCHAR(100) NOT NULL,
+  PRIMARY KEY (idCargo)
+);
+
+CREATE TABLE bitware_db.Funcionario (
+  idFuncionario INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(60) NOT NULL,
+  sobrenome VARCHAR(100) NOT NULL,
+  validado BIT(1),
+  fkCargo INT NOT NULL,
+  fkUsuario INT NOT NULL,
   fkEmpresa INT NOT NULL,
-  PRIMARY KEY (idCargo),
-  CONSTRAINT fk_Cargo_Empresa
+  PRIMARY KEY (idFuncionario),
+  CONSTRAINT fk_Funcionario_Cargo
+    FOREIGN KEY (fkCargo)
+    REFERENCES bitware_db.Cargo (idCargo),
+  CONSTRAINT fk_Usuario_Funcionario
+    FOREIGN KEY (fkUsuario)
+    REFERENCES bitware_db.Usuario (idUsuario),
+  CONSTRAINT fk_Empresa_Funcionario
     FOREIGN KEY (fkEmpresa)
     REFERENCES bitware_db.Empresa (idEmpresa)
 );
 
-CREATE TABLE bitware_db.Funcionario (
-  idUsuario INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(60) NOT NULL,
-  sobrenome VARCHAR(100) NULL,
-  email VARCHAR(200) NOT NULL UNIQUE,
-  senha VARCHAR(200) NOT NULL,
-  foto VARCHAR(100),
-  validado BIT(1),
-  fkCargo INT NOT NULL,
-  fkEmpresa INT NOT NULL,
-  PRIMARY KEY (idUsuario),
-  CONSTRAINT fk_Usuario_Cargo
-    FOREIGN KEY (fkCargo)
-    REFERENCES bitware_db.Cargo (idCargo),
-  CONSTRAINT fk_Funcionario_Empresa
-    FOREIGN KEY (fkEmpresa)
-    REFERENCES bitware_db.Empresa (idEmpresa)
-);
+
 
