70b97311b803ae47ed1de83640130029851c08cf
lukascomka <lukas.souza@sptech.school>
2025-09-04 22:57:31 -0300

    Atualizando nome de tabelas na modal

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -16,7 +16,7 @@ function cadastrar(nome, sobrenome, email, senha, fkCargo, fkEmpresa) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO funcionario (nome, sobrenome, email, senha, fkCargo, fkEmpresa) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}', '${fkCargo}', '${fkEmpresa}');
+        INSERT INTO Funcionario (nome, sobrenome, email, senha, fkCargo, fkEmpresa) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}', '${fkCargo}', '${fkEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
