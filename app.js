7050a1a59681822832a11c621d4de6368678da23
LPAmorim <lucas.santos058@sptech.school>
2025-11-30 16:01:46 -0300

    feat: add dashboard process routes and update HTML structure for KPI display

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var maquinaRouter = require("./src/routes/maquina")
 //var emailRouter = require("./src/routes/emails");
 var cadastrarEmpresa = require("./src/routes/empresas");
 var chamadosRouter = require("./src/routes/chamados");
+var procesKpiRouter = require("./src/routes/Tecnico/dashProcess");
 
 // Middlewares
 app.use(express.json());
@@ -46,6 +47,7 @@ app.use("/empresas", cadastrarEmpresa);
 app.use("/solicitacoes", solicitacaoRouter);
 app.use("/maquina", maquinaRouter)
 app.use("/chamados", chamadosRouter);
+app.use("/dashProcess", procesKpiRouter)
 //app.use("/emails", emailRouter);
 
 // Inicia o servidor
