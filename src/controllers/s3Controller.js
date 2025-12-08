33a5db54753827f5cb4499ed0516f818a07cf990
gabrielavmxx <gabriela.montero@sptech.school>
2025-12-08 10:51:50 -0300

    mudança na tela dashAnalista

--- src/controllers/s3Controller.js
+++ src/controllers/s3Controller.js
@@ -105,7 +105,7 @@ async function pegarCsvMedias(req, res) {
         }
         const key = `${idEmpresa}/medias/medias.csv`;
 
-        const bucket = "s3-client-bitwarepi" //bucket-client-2111
+        const bucket = "s3-client-bitwarepi" //s3-client-bitwarepi-reserva
         const data = await s3.getObject({
             Bucket: bucket,
             Key: key
@@ -231,7 +231,7 @@ async function pegarLeiturasFormatadas(req, res) {
         const periodo = Number(req.query.periodo) || 1;
 
         const key = `${idEmpresa}/leiturasFormatadas/leituras.csv`;
-        const bucket = "s3-client-bitwarepi"; 
+        const bucket = "s3-client-bitwarepi"; //s3-client-bitwarepi-reserva 
 
         const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
         const text = data.Body.toString("utf-8").trim();
